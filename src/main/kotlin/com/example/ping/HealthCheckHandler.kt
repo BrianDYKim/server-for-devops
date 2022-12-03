f88803eac401ac7fce4a48f98f0d07b69c7ed366
BrianDYKim <18k7102dy@naver.com>
2022-12-03 18:59:27 +0900

    log check

--- src/main/kotlin/com/example/ping/HealthCheckHandler.kt
+++ src/main/kotlin/com/example/ping/HealthCheckHandler.kt
@@ -1,5 +1,6 @@
 package com.example.ping
 
+import org.slf4j.LoggerFactory
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
@@ -14,7 +15,11 @@ import org.springframework.web.reactive.function.server.bodyValueAndAwait
 @Component
 class HealthCheckHandler {
 
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
     suspend fun healthCheck(request: ServerRequest): ServerResponse {
+        logger.info("health check!")
+
         return ok()
             .contentType(MediaType.APPLICATION_JSON)
             .bodyValueAndAwait("healthy!")
